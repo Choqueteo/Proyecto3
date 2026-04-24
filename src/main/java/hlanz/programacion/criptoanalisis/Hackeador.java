ceb66bbda12439f4b10e35091fee908d9ad3ccec
Alberto573 <albertmore65@gmail.com>
2026-04-24 20:08:01 +0200

    Cambio en Hackeador

--- src/main/java/hlanz/programacion/criptoanalisis/Hackeador.java
+++ src/main/java/hlanz/programacion/criptoanalisis/Hackeador.java
@@ -1,7 +1,5 @@
 package hlanz.programacion.criptoanalisis;
 
-import hlanz.programacion.criptografia.general.Descifrador;
-
 public interface Hackeador{
     public abstract HackResult descrifrar(String texto);
 }
